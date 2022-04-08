2424f340d8504e3f757847d1ed3ba015030c5564
Yesu G <50919263+yesu4658@users.noreply.github.com>
2022-04-08 21:01:23 +0530

    Update credentials.js

--- api/credentials.js
+++ api/credentials.js
@@ -8,5 +8,5 @@ export const keys = {
     auth_uri: process.env.AUTH_URI,
     token_uri: process.env.TOKEN_URI,
     auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_CURL,
-    client_x509_cert_url: process.env.CLIENT_X509_CERT_UR,
+    client_x509_cert_url: process.env.CLIENT_X509_CERT_URI,
 };
