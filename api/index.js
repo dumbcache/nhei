7923c54323a937c3bac65c2e7a198d10dc4482bd
dumbcache <50919263+dumbcache@users.noreply.github.com>
2022-11-03 11:20:11 +0530

    removed ui folder. 1 file modified, 40 deleted

--- api/index.js
+++ api/index.js
@@ -1,53 +1,58 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 
 import {
-    searchHandler,
-    getSectionsHandler,
-    getBoardsHandler,
-    getDoujinHandler,
-    searchCacheHandler,
-    createHandler,
-    createBoardHandler,
-    createSectionHandler,
-    deleteBoardHandler,
-    deleteSectionHandler,
-    createPinHandler,
-    deletePinHandler,
-    editSectionHandler,
-    editBoardHandler,
-    thumbsHandler,
-} from "./handlers.js";
+  searchHandler,
+  getSectionsHandler,
+  getBoardsHandler,
+  getDoujinHandler,
+  searchCacheHandler,
+  createHandler,
+  createBoardHandler,
+  createSectionHandler,
+  deleteBoardHandler,
+  deleteSectionHandler,
+  createPinHandler,
+  deletePinHandler,
+  editSectionHandler,
+  editBoardHandler,
+  thumbsHandler,
+} from './handlers.js';
+
+console.log('working');
 
 let app = express(),
-    port = 8080;
+  port = 8080;
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send({ status: "nhei running" });
+app.get('/', (req, res) => {
+  res.send({ status: 'nhei running' });
 });
-app.post("/search", searchCacheHandler, searchHandler);
-app.post("/doujin", getDoujinHandler);
-app.post("/create", createHandler);
-app.get("/thumbs", thumbsHandler);
+app.post('/search', searchCacheHandler, searchHandler);
+app.post('/doujin', getDoujinHandler);
+app.post('/create', createHandler);
+app.get('/thumbs', thumbsHandler);
 
-app.route("/boards")
-    .get(getBoardsHandler)
-    .post(createBoardHandler)
-    .patch(editBoardHandler)
-    .delete(deleteBoardHandler);
+app
+  .route('/boards')
+  .get(getBoardsHandler)
+  .post(createBoardHandler)
+  .patch(editBoardHandler)
+  .delete(deleteBoardHandler);
 
-app.route("/:board/sections")
-    .get(getSectionsHandler)
-    .put(createSectionHandler)
-    .delete(deleteSectionHandler)
-    .patch(editSectionHandler);
+app
+  .route('/:board/sections')
+  .get(getSectionsHandler)
+  .put(createSectionHandler)
+  .delete(deleteSectionHandler)
+  .patch(editSectionHandler);
 
-app.route("/pins")
-    .post(createPinHandler)
-    .patch(createPinHandler)
-    .delete(deletePinHandler);
+app
+  .route('/pins')
+  .post(createPinHandler)
+  .patch(createPinHandler)
+  .delete(deletePinHandler);
 
 app.listen(port, () => console.log(`listening at port ${port}`));
